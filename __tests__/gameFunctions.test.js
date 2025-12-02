0e293f8ec9285df04cffc4be9d585dca940a3ad1
unknown <igl.myrick@gmail.com>
2025-12-02 14:33:17 -0800

    add second test for rollCrit function

--- __tests__/gameFunctions.test.js
+++ __tests__/gameFunctions.test.js
@@ -6,4 +6,10 @@ describe("rollCrit", () => {
   test("should return a number", () => {
     expect(typeof rollResult).toEqual("number");
   });
+
+  test("should return a number between 1 and 10", () => {
+    
+    expect(rollResult).toBeGreaterThan(0);
+    expect(rollResult).toBeLessThan(11);
+  });
 });
