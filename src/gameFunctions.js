994c53df0de04afa032c420a3099f502cfc9a0eb
unknown <igl.myrick@gmail.com>
2025-12-03 13:42:13 -0800

    add handleCombat function

--- src/gameFunctions.js
+++ src/gameFunctions.js
@@ -2,3 +2,8 @@ export const rollCrit = () => {
   const result = Math.floor(Math.random() * 10) + 1;
   return result;
 }
+
+export const handleCombat = (atk, def) => {
+  const result = (atk / def) * 3;
+  return result;
+}
