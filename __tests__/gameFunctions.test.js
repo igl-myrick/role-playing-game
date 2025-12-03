1ed5ae47d15079e889d433e5773b59bfce8bf472
unknown <igl.myrick@gmail.com>
2025-12-02 16:03:04 -0800

    add first test for handleCombat function

--- __tests__/gameFunctions.test.js
+++ __tests__/gameFunctions.test.js
@@ -1,4 +1,4 @@
-import { rollCrit } from '../src/gameFunctions.js';
+import { handleCombat, rollCrit } from '../src/gameFunctions.js';
 
 describe("rollCrit", () => {
   const rollResult = rollCrit();
@@ -11,5 +11,15 @@ describe("rollCrit", () => {
     
     expect(rollResult).toBeGreaterThan(0);
     expect(rollResult).toBeLessThan(11);
+  });
+});
+
+describe("handleCombat", () => {
+  const playerAtk = 60;
+  const opponentDef = 20;
+  const combatResult = handleCombat(playerAtk, opponentDef);
+
+  test("should return a number", () => {
+    expect(typeof combatResult).toEqual("number");
   });
 });
