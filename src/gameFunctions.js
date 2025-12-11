a0f22d87cb6777c3059abdeee4a73edee30bfed3
unknown <igl.myrick@gmail.com>
2025-12-10 16:02:42 -0800

    move game functions to gameData.js

--- src/gameFunctions.js
+++
@@ -1,9 +0,0 @@
-export const rollCrit = () => {
-  const result = Math.floor(Math.random() * 10) + 1;
-  return result;
-}
-
-export const rollCombat = (atk, def) => {
-  const result = (atk / def) * 3;
-  return result;
-}
